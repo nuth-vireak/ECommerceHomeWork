1cbc58b6ac747cd2d98ee646f8442ec7c31ff463
nuth-vireak <timberr.4t@gmail.com>
2024-09-01 15:44:19 +0700

    Initial Commit

---
+++ ECommerce/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ECommerce
+//
+//  Created by KAK-REAK on 1/9/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ECommerce/ECommerceApp.swift
@@ -0,0 +1,17 @@
+//
+//  ECommerceApp.swift
+//  ECommerce
+//
+//  Created by KAK-REAK on 1/9/24.
+//
+
+import SwiftUI
+
+@main
+struct ECommerceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
